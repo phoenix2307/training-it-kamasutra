941f2f8b3c3cf963eca32e169a6b16e37edc69db
Alex <phoenix.trade2307@gmail.com>
2021-10-12 23:28:02 +0300

    task6

---
+++ freeCodeCamp/task6.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+
+var remainder = 11 % 3;
+console.log(remainder);
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
