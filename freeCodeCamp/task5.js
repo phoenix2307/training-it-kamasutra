64b6e30977b5018f89a3e0af82dd83a8818d3f11
Alex <phoenix.trade2307@gmail.com>
2021-10-12 23:10:39 +0300

    task5

---
+++ freeCodeCamp/task5.js
@@ -0,0 +1,12 @@
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+var myDecimal = 5.7;
+
+// Only change code below this line
+
+var product = 2.0 * 2.5;
+
+var quotient = 4.4 / 2.0; // Change this line
