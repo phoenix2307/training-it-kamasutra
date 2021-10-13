f5b70e5690f8767c338120a7449a9409f51a51c8
Alex <phoenix.trade2307@gmail.com>
2021-10-14 01:52:21 +0300

    day2

---
+++ codeWars/simpleMultiplication.js
@@ -0,0 +1,11 @@
+function simpleMultiplication(number) {
+    // your code........
+  if (number % 2 === 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
+}
+
+let num = simpleMultiplication(5);
+console.log(num);
