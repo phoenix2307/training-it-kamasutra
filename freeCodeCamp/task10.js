574b5db7c9350b01cd8643d517aca1148ec64ba7
Alex <phoenix.trade2307@gmail.com>
2021-10-14 01:33:43 +0300

    task10

---
+++ freeCodeCamp/task10.js
@@ -0,0 +1,8 @@
+var myFirstName = 'Alex';
+var myLastName = 'Vatamaniuk';
+
+var sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+console.log(sampleStr);
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(myStr);
