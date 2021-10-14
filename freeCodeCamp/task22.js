a4f798bf4bb74e99b3f7be0b7cb6c9c660383bb6
Alex <phoenix.trade2307@gmail.com>
2021-10-15 01:36:16 +0300

    task22

---
+++ freeCodeCamp/task22.js
@@ -0,0 +1,6 @@
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+console.log(lastLetterOfLastName);
