9d8d9149cb131976e8cc58c1764f768d3c59e574
Alex <phoenix.trade2307@gmail.com>
2021-10-15 20:21:38 +0300

    task34

---
+++ freeCodeCamp/task34.js
@@ -0,0 +1,8 @@
+var myList = [];
+myList.push(['apples', 15]);
+myList.push(['bread', 3]);
+myList.push(['cheese' , 4]);
+myList.push(['beer', 2]);
+myList.push(['milk', 3]);
+
+console.log(myList);
