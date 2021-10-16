73a6a28c651f59d43727d642a968ae9cffb1cf24
Alex <phoenix.trade2307@gmail.com>
2021-10-16 17:27:45 +0300

    square

---
+++ codeWars/square.js
@@ -0,0 +1,10 @@
+const square = arg => arg * arg;
+let s = square(5);
+
+console.log(s);
+
+// Более универсаотный вариант для разных степеней
+/*
+const square = arg => arg ** 2;
+let s = square(5);
+*/
