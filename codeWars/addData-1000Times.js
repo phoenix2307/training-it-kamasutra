64a551be911da3355363700419c0be120cd98aa1
Alex <phoenix.trade2307@gmail.com>
2021-10-17 10:26:52 +0300

    addData-1000Times

---
+++ codeWars/addData-1000Times.js
@@ -0,0 +1,11 @@
+// Best practices
+var websites = [];
+while (websites.length < 1000) websites.push("codewars");
+
+//My solution
+var websites2 = [];
+for (let i=0; i<1000; i++) {
+    websites2.push('codewars');
+}
+
+console.log(websites2);
