58a9ece99b619dedf5b1c9be3be677dda9ded904
Alex <phoenix.trade2307@gmail.com>
2021-10-18 22:19:55 +0300

    opposite-number

---
+++ codeWars/opposite-number.js
@@ -0,0 +1,12 @@
+//Best practices
+function opposite(number) {
+    return(-number);
+}
+//or const opposite = number => -number;
+
+//My solution
+function opposite(number) {
+    //your code here
+    number = number * -1;
+    return number;
+}
