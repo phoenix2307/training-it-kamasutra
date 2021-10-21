6d9fdc2fa26f3c70aa0c627db34d4ab7920fc6fa
Alex <phoenix.trade2307@gmail.com>
2021-10-21 12:06:05 +0300

    help

--- codeWars/NO-COMPLETED-count-ineger-in-two-arrays.js
+++ codeWars/NO-COMPLETED-count-ineger-in-two-arrays.js
@@ -1,17 +1,27 @@
-// function countPositivesSumNegatives(input) {
-//     let result = [];
-//     if (input != null || input.length > 0) {
-//         const positiv = input.filter(item => item > 0).length;
-//         const negativ = input.filter(item => item < 0).reduce((total, current) => total + current);
-//         result = [positiv, negativ];
-//     }
-//     return result;
+/*
+Description:
+Given an array of integers.
 
-// }
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+If the input array is empty or null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+//Best practices
+function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
+
+
+
+//Моя версия выдает ошибки
 
 function countPositivesSumNegatives(input) {
     let result = [];
-    if (input.length == 0 || input === null || input === undefined || input === false){
+    if (input === 0 || input === null){
         return result;
     } else {
         const positiv = input.filter(item => item > 0).length;
