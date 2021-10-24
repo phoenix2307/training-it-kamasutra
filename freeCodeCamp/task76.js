cd5be5312088fa00a110d4fd9165e16c12519dd7
Alex <phoenix.trade2307@gmail.com>
2021-10-24 14:17:07 +0300

    task76

---
+++ freeCodeCamp/task76.js
@@ -0,0 +1,23 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Daft Punk",
+      "title": "Homework",
+      "release_year": 1997,
+      "formats": [ 
+          "CD",
+          "Cassette",
+          "LP"
+        ]
+    }
+  ];
