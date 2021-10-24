2ef876f4d48f6e2cf46eebff8d8021dccaf3af68
Alex <phoenix.trade2307@gmail.com>
2021-10-24 12:35:51 +0300

    task67

---
+++ task67.js
@@ -0,0 +1,9 @@
+var myDog = {
+    // Only change code below this line
+    name: 'Lord',
+    legs: 4,
+    tails: 1,
+    friends: ['bones', 'ball']
+    
+    // Only change code above this line
+};
