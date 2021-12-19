b2c2be92e04772d22cdf78c96a318ba788230490
Alex <phoenix.trade2307@gmail.com>
2021-12-19 12:28:02 +0200

    math functions

--- Practice/mathFunction.js
+++ Practice/mathFunction.js
@@ -1,4 +1,6 @@
-//
+//Работа с математическими функциями. Урок№ 6. http://old.code.mu/tasks/javascript/base/rabota-s-matematicheskimi-funkciyami-v-javascript.html
+
+
 /*console.log('Округления')
 let number = 379
 let sqrt = Math.sqrt(number)
@@ -7,10 +9,6 @@ console.log(Math.ceil(sqrt))
 console.log(Math.floor(sqrt))
 console.log(sqrt.toFixed(3))
 console.log(sqrt.toPrecision(3))*/
-
-console.log('*************************')
-
-
 //
 
 /*
@@ -23,40 +21,6 @@ if (x % y === 0) {
 }
 */
 
-//
-// Работа со строками: split, join, reverse, String(), Number()
-
-//split
-/*Метод split осуществляет разбиение строки в массив по указанному разделителю.
-
-  Разделитель указывается первым необязательным параметром. Если он не задан - вернется вся строка. Если он задан как пустые кавычки '' - то каждый символ строки попадет в отдельный элемент массива.
-
-  Вторым необязательным параметром можно указать максимальное количество элементов в получившемся массиве (см. примеры).*/
-
-/*
-let str2 = 'html-css-javascript';
-console.log(str2.split('', 4))
-
-let str = '123456789';
-let newStr = str.split('')
-  .reverse()
-  .join('')
-console.log(newStr)
-
-//сумма чисел из строки
-let str5 = '12345'
-let arr = str5.split('')
-let sum = 0
-for (let i = 0; i < arr.length; i++) {
-  sum += Number(arr[i])
-}
-console.log(sum)
-
-let num = 12345
-let strNum = String(num)
-console.log(strNum.split(''))
-let num2 = Number(strNum)
-console.log(num2)*/
 
 //!!!! метод определения максимума из массива чисел
 let arr = [-5, 1, 2, 3, 4, 6, 12, 6, 32]
@@ -80,10 +44,10 @@ for (let i = 0; i < 4; i++) {
   let newNumber = Math.floor(Math.random() * ((100 - 1) + 1) + 1)
   arrRandomNumber.push(newNumber)
 
-    }
+}
 console.log(arrRandomNumber)
 let sum2 = 0
-for (let c = 0; c <arrRandomNumber.length; c++) {
+for (let c = 0; c < arrRandomNumber.length; c++) {
   sum2 += arrRandomNumber[c]
 }
 console.log(sum2)
@@ -98,7 +62,7 @@ const testingArr = (minim, maxim, count) => {
     arrTest.push(newNum)
   }
 
-  for (let c = 0; c <arrTest.length; c++) {
+  for (let c = 0; c < arrTest.length; c++) {
     sumTest += arrTest[c]
   }
 
@@ -109,3 +73,10 @@ const testingArr = (minim, maxim, count) => {
 }
 
 testingArr(1, 10, 4)
+
+let arr12 = [12, 15, 20, 25, 59, 79]
+let sum12 = 0
+for (let i = 0; i < arr12.length; i++) {
+  sum12+= arr12[i]
+}
+console.log(sum12 / arr12.length)
